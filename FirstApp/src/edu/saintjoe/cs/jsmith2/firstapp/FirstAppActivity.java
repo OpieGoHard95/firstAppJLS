78909a191ca44bf0b11b5aa5e3bd0e847e690363
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-23 16:15:59 -0400

    created the skeleton

--- FirstApp/src/edu/saintjoe/cs/jsmith2/firstapp/FirstAppActivity.java
+++ FirstApp/src/edu/saintjoe/cs/jsmith2/firstapp/FirstAppActivity.java
@@ -1,37 +1,94 @@
-// Jared Smith First App
-// CMP 112
-// Begun on March 23, 2015
+// Two notes here:
+//   1. You must rename this and put in your username
+//   2. You must comment out this line to compile with "javac"
 package edu.saintjoe.cs.jsmith2.firstapp;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+/* App Skeleton
+ * Written by Brian Capouch 17 March 2015
+ * This app will serve as the basis for student project apps
+ */
+// App Skeleton
+// Forked from Capouch added to by Jared Smith
+// 3/18/2016
+// For class
+// Import directives tell Eclipse which components you intend to use
+import com.google.devtools.simple.runtime.components.Component;
+import com.google.devtools.simple.runtime.components.HandlesEventDispatching;
+import com.google.devtools.simple.runtime.components.android.Button;
+import com.google.devtools.simple.runtime.components.android.Form;
+import com.google.devtools.simple.runtime.components.android.Label;
+import com.google.devtools.simple.runtime.components.android.HorizontalArrangement;
 
-public class FirstAppActivity extends Activity {
+import com.google.devtools.simple.runtime.events.EventDispatcher;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_first_app);
-	}
+// This is the only class file for our app
+public class FirstAppActivity extends Form implements HandlesEventDispatching {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.first_app, menu);
-		return true;
-	}
+	
+	// We begin with constants, and "global settings" variables
+	// -- This app has none for now
+	
+	// Next are the UI widget references
+	
+	// These objects are equivalent to "components" of App Inventor
+	// The containers which organize the app screen
+	private HorizontalArrangement line1;
+	private HorizontalArrangement line2;
+	private HorizontalArrangement line3;
+	private HorizontalArrangement line4;
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-}
+	// Next our two components
+	private Button dumbButton;
+	private Label resultLabel;
+	private Button chaacButton;
+	private Label chaacLabel;
+
+ // Java Bridger apps all use $define() in place of main()
+ void $define() {
+ 	
+ 	// Code in this block is equivalent to the "Designer" part of App Inventor
+    
+	 // "this" is an Activity object, or the "main screen"
+     this.BackgroundColor(COLOR_WHITE);
+     
+     // Create containers for UI widgets
+     line1 = new HorizontalArrangement(this);
+     line2 = new HorizontalArrangement(this);
+     line3 = new HorizontalArrangement(this);
+     line4 = new HorizontalArrangement(this);
+     
+     // Now create the user interface
+     dumbButton = new Button(line1,"Go ahead and push me ;)");   
+     resultLabel = new Label(line2,"");
+     chaacButton = new Button(line3, "Who is Chaac?");
+     chaacLabel = new Label(line4,"");
+     
+     
+     
+     // Let the runtime system know which events to report to the dispatcher
+     EventDispatcher.registerEventForDelegation(this, "ButtonClick", "Click");
+    
+ } // end $define()
+
+ // This method, known as a "callback" is invoked by the runtime system
+ // It will only be called when a delegated event occurs
+ @Override
+ public boolean dispatchEvent(Component component, String id, String eventName,
+         Object[] args) {
+ 	
+ 	// This code is equivalent to the "Blocks" part of App Inventor
+	    if (component.equals(dumbButton) && eventName.equals("Click")){
+	    	resultLabel.Text("See that wasn't so bad");
+	    	dumbButton.Text("I like to be pushed");
+	        return true;
+	     } // end dispatch '+' press
+	    if (component.equals(chaacButton) && eventName.equals("Click")){
+	    	chaacLabel.Text("THE RAIN GOD!");
+	    	return true;
+	    }
+	// This line is syntactically required
+    return true;
+	} // end dispatchEvent
+
+
+} // end activity class
